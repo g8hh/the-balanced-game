ce1c89c4b05db2d0f1019df99cff58f42a26a5c1
zhaolinxu <zhaolinxu@sina.cn>
2021-07-05 00:27:56 +0800

    汉化

--- js/saves.js
+++ js/saves.js
@@ -122,7 +122,7 @@ function exporty() {
 }
 
 function importy() {
-    let loadgame = prompt("Paste in your save WARNING: WILL OVERWRITE YOUR CURRENT SAVE")
+    let loadgame = prompt("粘贴您的存档。警告：这将覆盖您当前的游戏存档")
     if (loadgame != null) {
         load(loadgame)
         location.reload()
